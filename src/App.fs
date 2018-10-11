793b56174acbbde85ef1b4c9549e0c2d50038b89
Tobias Burger <tobias_burger@hotmail.com>
2018-10-11 17:14:34 +0200

    remove unneeded code, construct multiple trees, nicer drawings

--- src/App.fs
+++ src/App.fs
@@ -23,15 +23,20 @@ let radian (degree: float<degree>): float<radian> =
 let angle (Point (x1, y1)) (Point (x2, y2)): float<degree> =
     Math.Atan2(y2 - y1, x2 - x1) * (180.<degree> / Math.PI)
 
-let intersection (Circle (Point (x0, y0), r0)) (Circle (Point (x1, y1), r1)) =
+let distance (Point (x0, y0)) (Point (x1, y1)) =
     let dx = x1 - x0
     let dy = y1 - y0
-    let d = Math.Sqrt((dy*dy) + (dx*dx))
+    Math.Sqrt((dy*dy) + (dx*dx))
+
+let intersection (Circle (Point (x0, y0), r0)) (Circle (Point (x1, y1), r1)) =
+    let d = distance (Point (x0, y0)) (Point (x1, y1))
     if (d > (r0 + r1)) then
         None
     else if (d < Math.Abs(r0 - r1)) then
         None
     else
+        let dx = x1 - x0
+        let dy = y1 - y0
         let a = ((r0*r0) - (r1*r1) + (d*d)) / (2.0 * d)
         let x2 = x0 + (dx * a/d)
         let y2 = y0 + (dy * a/d)
@@ -55,9 +60,8 @@ let getAngles circle1 circle2 =
             Point (float x2, float y2)
         get circle1, get circle2)
 
-let hasIntersection circle1 circle2 =
-    intersection circle1 circle2
-    |> Option.isSome
+let hasIntersection (Circle (point0, r0)) (Circle (point1, r1)) =
+    distance point0 point1 < (r0 + r1)
 
 let removeIfParent intersections = [
     let parents = ResizeArray()
@@ -82,24 +86,30 @@ let intersections circles =
     |> removeIfParent // remove sub elements if element exists in parent
     |> List.filter (snd >> List.isEmpty >> not) // remove elements with empty sub circles
 
-let rec construct = function
-    | ((circle, inters)::other) ->
-        Some (Element (circle, List.map (fun c ->
-            let subs =
-                other
-                |> List.tryFind (fst >> (=)c)
-                |> Option.bind (List.singleton >> construct)
-            match subs with
-            | Some (Element (c', _) as x) when c' = c -> x
-            | Some subs -> Element (c, [subs])
-            | None -> Element (c, [])
-        ) inters))
-    | [] -> None
-
+let rec constructTree =
+    let rec loop acc = function
+        | ((circle, inters)::other) ->
+            (Element (circle, List.map (fun c ->
+                let subs =
+                    other
+                    |> List.tryFind (fst >> (=)c)
+                    |> Option.toList
+                    |> loop acc
+                match subs with
+                | [Element (c', _) as x] when c' = c -> x
+                | subs -> Element (c, subs)
+                | [] -> Element (c, [])
+            ) inters))::acc
+        | [] -> []
+    loop []
 
 open Fable.Import.Browser
 
-let drawArc (ctx: CanvasRenderingContext2D) (color: string) (Circle(Point (x, y), r) as circle) (sa: float<radian>, ea: float<radian>) =
+let drawArc
+        (ctx: CanvasRenderingContext2D)
+        (color: string)
+        (Circle(Point (x, y), r) as circle)
+        (sa: float<radian>, ea: float<radian>) =
     do
         // cut out everything underneath
         ctx.globalCompositeOperation <- "destination-out"
@@ -113,8 +123,7 @@ let drawArc (ctx: CanvasRenderingContext2D) (color: string) (Circle(Point (x, y)
         ctx.fillStyle <- !^color
         ctx.arc(x, y, r, float sa, float ea)
         ctx.fill()
-        ctx.lineWidth <- 2.0
-        ctx.setLineDash(ResizeArray [|5.; 2.|])
+        ctx.lineWidth <- 1.0
         ctx.stroke()
 
 let drawArcWith ctx color baseCircle circle =
@@ -126,33 +135,16 @@ let drawArcWith ctx color baseCircle circle =
 
 let drawLine (ctx: CanvasRenderingContext2D) (Point (x1, y1)) (Point (x2, y2)) =
     ctx.beginPath()
-    ctx.lineWidth <- 2.0
+    ctx.lineWidth <- 1.0
     ctx.setLineDash(ResizeArray [|5.; 2.|])
     ctx.moveTo(x1, y1)
     ctx.lineTo(x2, y2)
     ctx.stroke()
 
-let drawIntersection ctx circle1 circle2 =
-    intersection circle1 circle2
-    |> Core.Option.iter (fun (p1, p2) ->
-        drawLine ctx p1 p2)
+let rec flatten (Element (circle, subs)) =
+    circle::(List.collect flatten subs)
 
-let drawAngle ctx circle1 circle2 =
-    match intersection circle1 circle2 with
-    | Some (x, y) ->
-        let draw circle =
-            let (Circle (Point (a, b), _)) = circle
-            let p = Point (a, b)
-            //let a1 = angle p x
-            //let a2 = angle p y
-            //printfn "%A" (abs a1, abs a2)
-            drawLine ctx p x
-            drawLine ctx p y
-        draw circle1
-        draw circle2
-    | None -> ()
-
-let init ccircles =
+let render ccircles =
     let canvas = document.querySelector(".view") :?> HTMLCanvasElement
 
     let ctx = canvas.getContext_2d()
@@ -161,7 +153,7 @@ let init ccircles =
 
     let intersections = intersections circles
 
-    let result = construct intersections
+    let result = constructTree intersections
 
     let find circle =
         ccircles |> List.find (fst >> (=)circle)
@@ -169,32 +161,49 @@ let init ccircles =
     let rec render baseCircle (Element (c, subs)) =
         let c, color = find c
         drawArcWith ctx color baseCircle c
-        drawIntersection ctx baseCircle c
         for Element (c', subs') in subs do
             let circle', color' = find c'
             drawArcWith ctx color' c circle'
-            drawIntersection ctx c circle'
             List.iter (render circle') subs'
 
     result
-    |> Core.Option.iter (fun (Element (c, subs)) ->
+    |> List.iter (fun (Element (c, subs) as result) ->
         let circle, color = find c
         drawArc ctx color circle (0.<radian>, radian 360.<degree>)
         List.iter (render circle) subs)
 
+    let flattenedResult = List.collect flatten result
+    let loneCircles =
+        circles |> List.fold (fun xs c ->
+            if flattenedResult |> List.contains c |> not then
+                c::xs
+            else
+                xs) []
+    for c in loneCircles do
+        let circle, color = find c
+        drawArc ctx color circle (0.<radian>, radian 360.<degree>)
+
 let time1 = performance.now()
 
-let ccircles = [
-    Circle (Point (280., 280.), radius = 100.), "rgba(255, 0, 0, 0.5)" // "red"
-    Circle (Point (200., 200.), radius =  70.), "rgba(0, 0, 255, 0.5)" //"blue"
-    Circle (Point (380., 330.), radius =  40.), "rgba(0, 255, 0, 0.5)" // "green"
-    Circle (Point (400., 360.), radius =  40.), "rgba(255, 255, 0, 0.5)" //"yellow"
-    Circle (Point (250., 360.), radius =  50.), "rgba(255, 0, 255, 0.5)" //"magenta"
-    Circle (Point (340., 220.), radius =  60.), "rgba(0, 255, 255, 0.5)" //"cyan"
-]
+type Input = { id: string; x: string; y: string }
 
-init ccircles
+let rndColor () =
+    let rnd = Random()
+    let next () = rnd.Next(128, 256) // only light colors
+    sprintf "rgb(%i,%i,%i)" (next()) (next()) (next())
+
+let ccircles =
+    importDefault<Input array> "./circles.json"
+    |> Array.map (fun i ->
+        let color = rndColor ()
+        Circle(Point(float i.x, float i.y), 10.), color)
+    |> List.ofArray
 
 let time2 = performance.now()
 
-console.log(sprintf "Time for render: %.0f milliseconds" (time2 - time1))
+render ccircles
+
+let time3 = performance.now()
+
+console.log(sprintf "Time for JSON-import: %.0f milliseconds" (time2 - time1))
+console.log(sprintf "Time for render: %.0f milliseconds" (time3 - time2))
